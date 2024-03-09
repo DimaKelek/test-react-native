673947fbd186080a6216a6c25c27a9e4518eb3cc
Dmitry Shelestov <kelekdev@gmail.com>
2024-03-09 10:58:19 +0300

    setup linter and aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React, { type ReactElement } from "react";
 import { SafeAreaView, Text } from "react-native";
+import { FLEX_CENTER } from "@commonStyles";
 
 export const App = (): ReactElement => {
   return (
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
+    <SafeAreaView style={{ flex: 1, ...FLEX_CENTER }}>
       <Text>{"App"}</Text>
     </SafeAreaView>
   );
